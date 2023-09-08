b17ffabe0985c0b1ad903768382e04beb229e2bd
qinwenya <2017668014@qq.com>
2023-09-08 12:28:02 +0800

    自动推送

---
+++ push.sh
@@ -0,0 +1,7 @@
+ git config --global user.name 'qinwenya'
+ git config --global user.email '2017668014@qq.com'
+ git config --global http.sslverify false
+ git add .
+ git commit -m '自动推送'
+ git sh push.sh
+ 
